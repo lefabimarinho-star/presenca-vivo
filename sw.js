6a1e728047074e0a31c185f35c14b20233ecb8d2
lefabimarinho-star <lefabimarinho@gmail.com>
2026-08-19 15:41:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,8 @@
-const CACHE_VERSION = 'presenca-vivo-v2';
-
-const APP_SHELL = [
-  './index.html',
-  './manifest.webmanifest',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_VERSION).then(cache => cache.addAll(APP_SHELL))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_VERSION)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
+const CACHE_VERSION = 'presenca-vivo-v3';
+const APP_SHELL = ['./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_VERSION).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_VERSION).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request)));
 });
